e4128ba56bd46c1b59762380609af3f3f7fe25a8
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-21 08:31:38 +0000

    updated next.config

--- little-lion/next.config.mjs
+++ little-lion/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  assetPrefix: '/', // Ensures CSS/JS assets load relative to your domain over HTTPS
 };
 
 export default nextConfig;
