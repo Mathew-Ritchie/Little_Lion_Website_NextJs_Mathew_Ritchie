caef029a0b523d41b55c926c5720af825f2c162a
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-24 11:46:39 +0000

    updated hero responsiveness.

--- little-lion/src/components/Hero.jsx
+++ little-lion/src/components/Hero.jsx
@@ -2,33 +2,31 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative h-screen w-full overflow-hidden flex flex-col md:flex-row-reverse md:gap-5 items-center justify-center bg-gray-800">
+    <section className="relative h-screen w-full pt-20 sm:pt-35 overflow-hidden flex flex-col md:flex-row-reverse md:px-10 md:gap-5 items-center justify-start sm:justify-center bg-gray-800">
       {/* Content */}
-      <div className="container text-center md:text-left text-white md:w-90">
-        <h1 className="text-5xl md:text-6xl font-extrabold mb-2">
+      <div className="text-center md:text-left text-white">
+        <h1 className="text-7xl font-extrabold mb-2">
           Little Lion
         </h1>
 
-        <h2 className="global-accent-text text-center md:text-left md:pr-25 text-3xl md:text-3xl font-extrabold mb-6">
+        <h2 className="global-accent-text text-center md:text-left md:pr-25 text-4xl md:text-4xl font-extrabold mb-2">
           Web Designs
         </h2>
 
-        <p className="text-xl md:text-2xl mb-8 lg:w-80">
+        <p className="text-2xl md:text-2xl mb-8 lg:w-80">
           Every Business Can Make A Big Impression
         </p>
       </div>
       <div className="flex flex-col items-center gap-2"> 
-        <Image
-        src="/images/little-lion-logo.png"
-        alt="Little Lion Logo of a Shih Tzu Dog"
-        className="w-5/6 md:w-1/3 lg:w-120 h-auto"
-        width={1536}
-        height={1024} 
-      />
-      <p className="text-white/50 text-xl italic">&quot;Shih Tzu&quot; - &quot;Little Lion&quot;</p>
-      
-      </div>
-     
+          <Image
+            src="/images/little-lion-logo.png"
+            alt="Little Lion Logo of a Shih Tzu Dog"
+            className="w-5/6 md:w-full lg:w-120 h-auto"
+            width={1536}
+            height={1024} 
+          />
+          <p className="text-white/50 text-xl italic">&quot;Shih Tzu&quot; - &quot;Little Lion&quot;</p>
+      </div>  
     </section>
   );
 }
