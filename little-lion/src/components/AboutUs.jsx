a34e81f34b7a2c23fdf289fb69f31e6dea4030ce
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-24 12:38:57 +0000

    AuboutU bg colour and and text shadow.

--- little-lion/src/components/AboutUs.jsx
+++ little-lion/src/components/AboutUs.jsx
@@ -14,18 +14,18 @@ export default function About() {
           backgroundImage: `url("/images/web-pic.png")`,
         }}
       ></div>
-      <div className=" w-full md:w-1/2 min-h-screen px-5 text-center md:text-start  bg-black/50 flex flex-col justify-center text-white">
+      <div className=" w-full md:w-1/2 min-h-screen px-5 text-center md:text-start  bg-black/60 flex flex-col justify-center text-white">
         <h1 className="global-accent-text text-shadow-lg text-shadow-black/20 text-6xl md:text-6xl font-extrabold mb-6 pt-20">About Us</h1>
-        <p className="text-2xl md:text-2xl mb-8 max-w-2xl mx-auto">
+        <p className="text-2xl md:text-2xl mb-8 max-w-2xl mx-auto text-shadow-lg text-shadow-black/20">
           Little Lion is dedicated to great web design and development, creating stunning,
           user-friendly websites that help businesses establish a strong online presence.
         </p>
-        <p className="text-xl md:text-xl mb-8 max-w-2xl mx-auto">
+        <p className="text-xl md:text-xl mb-8 max-w-2xl mx-auto text-shadow-lg text-shadow-black/20">
           Our aim is to provide high-quality, customized web solutions that not only look great but are
           also fully responsive, performing seamlessly across all devices. We believe that a well-designed website is
           crucial for any business looking to thrive in today&apos;s digital landscape.
         </p>
-        <p className="text-2xl md:text-xl mb-8 max-w-2xl italic mx-auto">
+        <p className="text-2xl md:text-xl mb-8 max-w-2xl italic mx-auto text-shadow-lg text-shadow-black/20">
           &quot;You are proud of your business, let us help you show it off to the world.&quot;
         </p>
       </div>
