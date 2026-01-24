cc403184426c62baa2e53128415dbd7bfcf11d5a
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-24 12:32:27 +0000

    hight change AboutUs.

--- little-lion/src/components/AboutUs.jsx
+++ little-lion/src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section
       id="about-us"
-      className="about-section w-full h-auto relative overflow-hidden flex items-center justify-center md:justify-end before:content-[''] before:absolute before:inset-0 before:bg-gray-200 before:-z-20"
+      className="about-section w-full min-h-screen relative overflow-hidden flex items-center justify-center md:justify-end before:content-[''] before:absolute before:inset-0 before:bg-gray-200 before:-z-20"
     >
       <div
         className="absolute inset-0 bg-no-repeat -z-10 bg-size-[100%] lg:bg-size-[60%] bg-fixed bg-left md:bg-left"
@@ -14,7 +14,7 @@ export default function About() {
           backgroundImage: `url("/images/web-pic.png")`,
         }}
       ></div>
-      <div className=" w-full md:w-1/2 h-auto px-5 text-center md:text-start  bg-black/50 flex flex-col justify-center text-white">
+      <div className=" w-full md:w-1/2 min-h-screen px-5 text-center md:text-start  bg-black/50 flex flex-col justify-center text-white">
         <h1 className="global-accent-text text-shadow-lg text-shadow-black/20 text-6xl md:text-6xl font-extrabold mb-6 pt-20">About Us</h1>
         <p className="text-2xl md:text-2xl mb-8 max-w-2xl mx-auto">
           Little Lion is dedicated to great web design and development, creating stunning,
